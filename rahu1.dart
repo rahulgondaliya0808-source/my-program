20e0e908ff3027b781287605ad289529bc98e1b7
rahulgondaliya0808-source <rahulgondaliya0808@gmail.com>
2026-09-10 15:51:29 +0530

    Add files via upload

---
+++ rahu1.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+
+
+int a=10;
+double asl =10000;
+String nm = 'atmiya';
+bool std = true;
+
+
+print("integer -> $a");
+print("double -> $asl");
+print("string -> $nm");
+print("boolean -> $std");
+
+}
